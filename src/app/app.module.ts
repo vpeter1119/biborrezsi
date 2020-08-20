263150d7b9d16cae6859447137a09d7fbe7b9a48
Peter Vertesi <vpeter1119@gmail.com>
2020-08-20 12:43:26 +0200

    Add server-down and footer components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerDownComponent } from './server-down/server-down.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerDownComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
